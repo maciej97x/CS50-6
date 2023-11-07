d3014ea899083695958d83f5d10537e011eb2e74
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-07 08:27:03 +0000

    Tue, Nov 7, 2023, 8:27 AM +00:00

--- hello/app.py
+++ hello/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     if "name" in request.args:
-        name = request.args["names"]
+        name = request.args["name"]
     else:
         name = "world"
     return render_template("index.html")
