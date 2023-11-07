39974db501dd394944c2b42fa39471c6373efa21
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-07 12:36:16 +0000

    Tue, Nov 7, 2023, 12:36 PM +00:00

--- hello6/app.py
+++ hello6/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET"
+    if request.method == "GET":
         return render_template("index.html")
     elif request.method == "POST":
             return render_template("greet.html", name=request.args.get("name", "world"))
