ebffe37d47739603866df20e0ece185f40e33bef
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-07 12:35:04 +0000

    Tue, Nov 7, 2023, 12:35 PM +00:00

--- hello5/app.py
+++ hello5/app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method == "GET"
-        return render_template("index.html")
-    elif request.method == "POST":
-            return render_template("greet.html", name=request.args.get("name", "world"))
+    return render_template("index.html")
 
 
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
