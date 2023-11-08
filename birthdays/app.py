ee587a8ae64428e9cde4ae9539ab8afa377caf51
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-08 10:03:26 +0000

    Wed, Nov 8, 2023, 10:03 AM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -29,6 +29,10 @@ def index():
         month = request.form.get("month")
         day = request.form.get("day")
 
+
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (:name, :month, :day)",
+                   name=name, month=month, day=day)
+
         return redirect("/")
 
     else:
