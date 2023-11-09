ad910dbe8f6c8d07ac9eba3f2dc5206866015d0d
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-09 12:26:23 +0000

    Thu, Nov 9, 2023, 12:26 PM +00:00

--- finance/helpers.py
+++ finance/helpers.py
@@ -1,35 +1,58 @@
 import os
-import requests
 import urllib.parse
+import requests
 
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, session
+from werkzeug.security import check_password_hash
+from datetime import datetime
 from functools import wraps
 
 
 def apology(message, code=400):
     """Render message as an apology to user."""
+
     def escape(s):
         """
         Escape special characters.
+
         https://github.com/jacebrowning/memegen#special-characters
         """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
             s = s.replace(old, new)
         return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+    if isinstance(message, str):
+        message = escape(message)
+    else:
+        message = "Error occured!"
+
+    return render_template("apology.html", top=code, bottom=message), code
 
 
 def login_required(f):
     """
     Decorate routes to require login.
+
     https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
     """
+
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
+
         return f(*args, **kwargs)
+
     return decorated_function
 
 
@@ -51,7 +74,7 @@ def lookup(symbol):
         return {
             "name": quote["companyName"],
             "price": float(quote["latestPrice"]),
-            "symbol": quote["symbol"]
+            "symbol": quote["symbol"],
         }
     except (KeyError, TypeError, ValueError):
         return None
@@ -59,4 +82,18 @@ def lookup(symbol):
 
 def usd(value):
     """Format value as USD."""
+
     return f"${value:,.2f}"
+
+
+def get_time():
+    """Returns formatted local time."""
+
+    return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+
+def check_password(first_password, second_password):
+    """Checks the passwords to be a match. Returns a message error if they do not match."""
+
+    if not check_password_hash(first_password, second_password):
+        return apology("Passwords do not match!", code=401)
