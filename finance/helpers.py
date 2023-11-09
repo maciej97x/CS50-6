192fd5ee2fc5c6b07b43e011c2a19e6a0392f396
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-11-09 12:48:36 +0000

    Thu, Nov 9, 2023, 12:48 PM +00:00

--- finance/helpers.py
+++ finance/helpers.py
@@ -1,78 +1,99 @@
-import csv
-import datetime
-import pytz
+import os
+import urllib.parse
 import requests
-import subprocess
-import urllib
-import uuid
 
 from flask import redirect, render_template, session
+from werkzeug.security import check_password_hash
+from datetime import datetime
 from functools import wraps
 
 
 def apology(message, code=400):
     """Render message as an apology to user."""
+
     def escape(s):
         """
         Escape special characters.
 
         https://github.com/jacebrowning/memegen#special-characters
         """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
             s = s.replace(old, new)
         return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+    if isinstance(message, str):
+        message = escape(message)
+    else:
+        message = "Error occured!"
+
+    return render_template("apology.html", top=code, bottom=message), code
 
 
 def login_required(f):
     """
     Decorate routes to require login.
 
-    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
     """
+
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
+
         return f(*args, **kwargs)
+
     return decorated_function
 
 
 def lookup(symbol):
     """Look up quote for symbol."""
 
-    # Prepare API request
-    symbol = symbol.upper()
-    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
-    start = end - datetime.timedelta(days=7)
-
-    # Yahoo Finance API
-    url = (
-        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
-        f"?period1={int(start.timestamp())}"
-        f"&period2={int(end.timestamp())}"
-        f"&interval=1d&events=history&includeAdjustedClose=true"
-    )
-
-    # Query API
+    # Contact API
     try:
-        response = requests.get(url, cookies={"session": str(uuid.uuid4())}, headers={"User-Agent": "python-requests", "Accept": "*/*"})
+        api_key = os.environ.get("API_KEY")
+        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
+        response = requests.get(url)
         response.raise_for_status()
+    except requests.RequestException:
+        return None
 
-        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
-        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
-        quotes.reverse()
-        price = round(float(quotes[0]["Adj Close"]), 2)
+    # Parse response
+    try:
+        quote = response.json()
         return {
-            "name": symbol,
-            "price": price,
-            "symbol": symbol
+            "name": quote["companyName"],
+            "price": float(quote["latestPrice"]),
+            "symbol": quote["symbol"],
         }
-    except (requests.RequestException, ValueError, KeyError, IndexError):
+    except (KeyError, TypeError, ValueError):
         return None
 
 
 def usd(value):
     """Format value as USD."""
+
     return f"${value:,.2f}"
+
+
+def get_time():
+    """Returns formatted local time."""
+
+    return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+
+def check_password(first_password, second_password):
+    """Checks the passwords to be a match. Returns a message error if they do not match."""
+
+    if not check_password_hash(first_password, second_password):
+        return apology("Passwords do not match!", code=401)
